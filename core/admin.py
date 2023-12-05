e8ae2b7fcab390d9c5e5017337f253e60278838e
marco <marcotts@gmail.com>
2023-12-05 14:04:57 +0100

    update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Transcript, Video
+
+admin.site.register(Category)
+admin.site.register(Transcript)
+admin.site.register(Video)
+
+
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,9 @@ from . import views as mv
 
 
 urlpatterns = [
-    path('home', mv.home, name='home'),
+    path('home/', mv.home, name='home'),
+    path('video_list/<int:category_id>/', mv.video_list, name='video_list'),
+    path('video_detail/<int:video_id>/', mv.video_detail, name='video_detail'),
+    path('search/', mv.search, name='search'),
 ]
 
